e288115231aad89d5103cf0b9d65cbe640936944
SPrio <mahantasupriyo@gmail.com>
2021-02-08 13:24:40 +0530

    formatter applied

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
 import React, { Component } from "react";
-import ReactEcharts from "echarts-for-react";class App extends Component {
+import ReactEcharts from "echarts-for-react";
+class App extends Component {
   render() {
     return (
       <ReactEcharts
         option={{
           xAxis: {
             type: "category",
-            data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+            data: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
           },
           yAxis: {
-            type: "value"
+            type: "value",
           },
-          series: [{
-            data: [820, 932, 901, 934, 1290, 1330, 1320],
-            type: "line"
-          }]
+          series: [
+            {
+              data: [820, 932, 901, 934, 1290, 1330, 1320],
+              type: "line",
+            },
+          ],
         }}
       />
     );
   }
-}export default App;
+}
+export default App;
